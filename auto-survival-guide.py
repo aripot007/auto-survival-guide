5a9211798dcadaad7841a3dc324945ae75edd933
aripot007 <32022176+aripot007@users.noreply.github.com>
2022-02-03 22:56:09 +0100

    Add a template file

--- auto-survival-guide.py
+++ auto-survival-guide.py
@@ -3,15 +3,19 @@ import re
 import json
 import argparse
 import requests
+
 try:
     from tqdm import tqdm
+
     use_tqdm = True
 except ImportError:
     use_tqdm = False
 
 import settings
+from settings import TEMPLATE_PLACEHOLDERS as placeholders
 
 API_SERVER = "https://api.survival-guide.tk"
+
 
 def remove_html_tags(text):
     clean = re.compile('<.*?>')
@@ -67,13 +71,15 @@ ap = argparse.ArgumentParser()
 ap.add_argument("-f", "--file", help="path to guide.json")
 ap.add_argument("-l", "--latest", action="store_true", help="Get the latest survival guide from the internet")
 ap.add_argument("-t", "--template", help="Path to the template file", default=settings.TEMPLATE_PATH)
-ap.add_argument("-n", "--sentences-per-word", dest="sentences-per-word",help="How many sentences should be used for a word", default=settings.SENTENCES_PER_WORD)
+ap.add_argument("--test", help="Test run", action="store_true")
+ap.add_argument("-n", "--sentences-per-word", dest="sentences-per-word",
+                help="How many sentences should be used for a word", default=settings.SENTENCES_PER_WORD)
 ap.add_argument("--list", action="store_true", help="Choose from available guides on the server")
 ap.add_argument("-m", "--manual", help="Use manual mode", action="store_true")
-ap.add_argument("-s", "--skip-not-found", "--skip", dest="skip-not-found", required=False, help="Skip words with no information found", action="store_true")
+ap.add_argument("-s", "--skip-not-found", "--skip", dest="skip-not-found", required=False,
+                help="Skip words with no information found", action="store_true")
 ap.add_argument("-o", "--output", required=False, help="output file", default=settings.DEFAULT_OUTPUT)
 args = vars(ap.parse_args())
-
 
 # Demande le guide manuellement
 if args["manual"]:
@@ -101,6 +107,25 @@ else:
             print("Error while decoding the guide :")
             print(e)
             exit(1)
+
+    elif args["test"]:
+        guide = {"title": "Test Guide",
+                 "parts": [
+                     {
+                         "title": "Part 1 - Definitions",
+                         "words": [
+                             "Processor",
+                             "Binary"
+                         ],
+                         "sources": [2, 1]
+                     },
+                     {
+                         "title": "Part 2",
+                         "words": ["Hello world"],
+                         "sources": [1, 2]
+                     }
+                 ]
+                 }
 
     elif args["file"] != "":
         with open(args["file"], "r") as f:
@@ -139,7 +164,9 @@ for part in parts:
             # Wiktionary
             if s == 2:
                 try:
-                    search = requests.get("https://en.wiktionary.org/w/api.php?action=opensearch&limit=1&namespace=0&format=json&profile=fuzzy&redirects=resolve&search={}".format(word))
+                    search = requests.get(
+                        "https://en.wiktionary.org/w/api.php?action=opensearch&limit=1&namespace=0&format=json&profile=fuzzy&redirects=resolve&search={}".format(
+                            word))
                     search_result = json.loads(search.text)
                     page_title = search_result[1][0]
                     url = requests.get("https://en.wiktionary.org/api/rest_v1/page/definition/{}".format(page_title))
@@ -152,10 +179,14 @@ for part in parts:
             elif s == 1:
                 # Recherche du mot
                 try:
-                    search = requests.get("https://en.wikipedia.org/w/api.php?action=opensearch&limit=1&namespace=0&format=json&profile=fuzzy&redirects=resolve&search={}".format(word))
+                    search = requests.get(
+                        "https://en.wikipedia.org/w/api.php?action=opensearch&limit=1&namespace=0&format=json&profile=fuzzy&redirects=resolve&search={}".format(
+                            word))
                     search_result = json.loads(search.text)
                     article_title = search_result[1][0]
-                    url = requests.get("https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exsentences={}&exlimit=1&explaintext=1&formatversion=2&format=json&titles={}".format(sentence_per_word, article_title))
+                    url = requests.get(
+                        "https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exsentences={}&exlimit=1&explaintext=1&formatversion=2&format=json&titles={}".format(
+                            sentence_per_word, article_title))
                     data = json.loads(url.text)
                     reponse = data["query"]["pages"][0]["extract"]
                 except:
@@ -182,29 +213,32 @@ for part in parts:
 
 print("\nGenerating guide ...")
 
+with open(args["template"], "r") as f:
+    template = f.read()
 
-latex = ""
+template = template.replace(placeholders["guide_title"], titre_guide)
 
-for partie in parties:
-    titre = partie[0]
-    liste = partie[1]
+# Get the text corresponding to a part
+text_intro, part_template, text_outro = re.findall("(.*)" + placeholders["part_start"] + "(.*)" + placeholders["part_end"] + "(.*)", template, re.RegexFlag.DOTALL)[0]
 
-    latex += "\section*{"+titre+"}\n"
-    latex += "\\begin{itemize}\n"
+# Get the text corresponding to a word
+word_template = re.findall(placeholders["word_start"] + "(.*)" + placeholders["word_end"], part_template, re.RegexFlag.DOTALL)[0]
 
-    for mot, definition in liste.items():
-        latex += "\item \\textbf{"+mot+"} : {"+definition+"}\n"
+output = text_intro
+for part in parties:
+    title = part[0]
+    words = part[1]
+    words_text = ""
+    for w, d in words.items():
+        words_text += word_template.replace(placeholders["word_word"], w).replace(placeholders["word_def"], d)
 
-    latex += "\end{itemize}\n"
+    text = re.sub(placeholders["word_start"] + "(.*)" + placeholders["word_end"], words_text, part_template, flags=re.RegexFlag.DOTALL)
+    text = text.replace(placeholders["part_title"], title)
+    output += text
 
-latex += "\end{document}"
+output += text_outro
 
-template_file = open(args["template"], "r")
-template = template_file.read()
-template_file.close()
-
-final_file = open(args["output"], "w")
-final_file.write(template.replace("TITLE", titre_guide)+latex)
-final_file.close()
+with open(args["output"], "w") as f:
+    f.write(output)
 
 print("Done !")
--- settings.py
+++ settings.py
@@ -10,4 +10,17 @@ TEMPLATE_PATH = "template.txt"
 # The path to the tesseract OCR command
 TESSERACT_CMD = "tesseract"
 
+#############################################
+#           Template Placeholders           #
+#############################################
 
+TEMPLATE_PLACEHOLDERS = {
+    "guide_title": "GUIDE_TITLE",
+    "part_start": "PART_START",
+    "part_end": "PART_END",
+    "part_title": "PART_TITLE",
+    "word_start": "WORD_START",
+    "word_end": "WORD_END",
+    "word_word": "WORD_WORD",
+    "word_def": "WORD_DEF"
+}
